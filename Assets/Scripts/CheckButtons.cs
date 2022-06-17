36255c39b330fa0865fe78bfd669c688194c5e93
ivissivi <ivis31121999@gmail.com>
2022-06-17 14:37:57 +0300

    Added messages

--- Assets/Scripts/CheckButtons.cs
+++ Assets/Scripts/CheckButtons.cs
@@ -10,19 +10,52 @@ public class CheckButtons : MonoBehaviour
     [SerializeField] private GameObject button3;
 
     [SerializeField] private Text finishedGame;
+    [SerializeField] private Text timer;
+
+    [SerializeField] public int limit;
+
+    private int startTime;
+    private int score;
+    public bool startCountdown = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        finishedGame.text = "yes";
+        finishedGame.text = "";
+        startTime = limit;
+    }
+
+    IEnumerator countdownClock()
+    {
+        startCountdown = true;
+        yield return new WaitForSeconds(1);
+        limit -= 1;
+        timer.text = "Time left: " + limit.ToString();
+        startCountdown = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(button1 && button2 && button3)
+    {   
+        score = startTime - limit;
+        if (startCountdown == false && limit > 0)
         {
-            finishedGame.text ="yes";
+            StartCoroutine(countdownClock());
+        }
+
+        if(button1.active && button2.active && button3.active && limit > 0)
+        {
+                finishedGame.color = Color.green;
+                finishedGame.text ="COMPLETED! Your time was " + score + " seconds!";
+                timer.text = "";
+                StopCoroutine(countdownClock());
+        } 
+
+        if(limit <= 0)
+        {
+            finishedGame.color = Color.red;
+            finishedGame.text ="FAILED!";
+            StopCoroutine(countdownClock());
         }
     }
 }
